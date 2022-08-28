f73cf047ed39638293b64ad35fd5604729f1e8ee
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-08-28 21:18:16 +0900

    feat: add blog

--- _config.js
+++ _config.js
@@ -6,6 +6,7 @@ import date from "lume/plugins/date.ts";
 import jsx from "lume/plugins/jsx.ts";
 import base_path from "lume/plugins/base_path.ts";
 import attributes from "lume/plugins/attributes.ts";
+import code_highlight from "lume/plugins/code_highlight.ts";
 
 const site = lume();
 
@@ -14,6 +15,7 @@ site.copy("img");
 site.copy("CNAME");
 site.use(attributes());
 site.use(base_path());
+site.use(code_highlight());
 site.use(date());
 site.use(jsx());
 site.use(parcel_css());
